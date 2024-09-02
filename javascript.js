a14d8b911d149da3f1e37170d438b27c9d7c2e9f
Vishnu <vishnupa@umich.edu>
2024-09-02 00:38:06 -0400

    Add function logic for numbers and operations
    The logic for the functions necessary for numbers and operations is now
    present in javascript file. It has only been implemented for button 1
    and operation add. Both the display part and button function has been
    combined into one function.

--- javascript.js
+++ javascript.js
@@ -3,19 +3,80 @@ let operation = undefined;
 let secondNumber = undefined;
 
 const display = document.querySelector(".numbers");
+display.textContent = "88888";
 
-function assign(value) {
+const one = document.querySelector(".one");
+one.addEventListener("click", () => {
     if (firstNumber === undefined) {
-        firstNumber = value;
+        firstNumber = 1;
+        display.textContent = firstNumber;
     } else if (operation === undefined) {
-        firstNumber = (firstNumber*10) + value;
+        firstNumber = (firstNumber*10) + 1;
+        display.textContent = firstNumber;
     } else if (secondNumber === undefined) {
-        secondNumber = value;
+        secondNumber = 1;
+        display.textContent = firstNumber + operation + secondNumber;
     } else {
-        secondNumber = (secondNumber*10) + value;
+        secondNumber = (secondNumber*10) + 1;
+        display.textContent = firstNumber + operation + secondNumber;
     }
+});
+
+const plus = document.querySelector(".plus");
+plus.addEventListener("click", () => {
+    operation = "+";
+    display.textContent = firstNumber + operation;
+});
+
+
+// function assign(value) {
+//     display.textContent = "1";
+    // if (firstNumber === undefined) {
+    //     firstNumber = value;
+    // } else if (operation === undefined) {
+    //     firstNumber = (firstNumber*10) + value;
+    // } else if (secondNumber === undefined) {
+    //     secondNumber = value;
+    // } else {
+    //     secondNumber = (secondNumber*10) + value;
+    // }
 }
 
-const btn1 = document.querySelector(".one");
-btn1.addEventListener("click", assign(1));
-btn1.addEventListener("click", () => display.textContent = firstNumber);
+// const clr = document.querySelector(".clear");
+// clr.addEventListener("click", clear());
+
+// function clear() {
+//     firstNumber = undefined;
+//     operation = undefined;
+//     secondNumber = undefined;
+// }
+
+// function displayNumbers() {
+//     if (secondNumber !== undefined) {
+//         display.textContent = "";
+//         display.textContent = firstNumber + operation + secondNumber;
+//     } else if (operation !== undefined) {
+//         display.textContent = firstNumber + operation;
+//     } else if (firstNumber !== undefined) {
+//         display.textContent = firstNumber;
+//     } else {
+//         display.textContent = "88888";
+//     }
+// }
+
+// const one = document.querySelector(".one");
+// one.addEventListener("click", assign(1));
+// one.addEventListener("click", displayNumbers());
+
+// const two = document.querySelector(".two");
+// two.addEventListener("click", assign(2));
+// two.addEventListener("click", displayNumbers());
+
+// const four = document.querySelector(".four");
+// four.addEventListener("click", assign(4));
+
+// const three = document.querySelector(".three");
+// three.addEventListener("click", assign(3));
+
+// const clr = document.querySelector(".clear");
+// clr.addEventListener("click", clear());
